d8f1933716b38ac3560b268588d791ffbd78ca5b
Jabir <220501@student.glu.nl>
2023-02-16 00:41:51 +0100

    commit

--- challenge-4/script.js
+++ challenge-4/script.js
@@ -1,7 +1,20 @@
 const container = document.getElementById("container");
+let height = screen.height * 2;
 
-for (i = 0; i < 1000; i++) {
-    const block = document.createElement("div")
-    block.className = "block"
-    container.appendChild(block)
+for (i = 0; i < height; i++) {
+  const block = document.createElement("div");
+  block.className = "block";
+  container.appendChild(block);
 }
+
+let activities = [
+  ["Work", 9],
+  ["Eat", 1],
+  ["Commute", 2],
+  ["Play Game", 1],
+  ["Sleep", 7],
+];
+activities.push(['Study',2]);
+
+console.table(activities);
+console.log(activities[0][1]);
