bad39439a7885561c71e18d02fa80e30931b8565
Jabir <220501@student.glu.nl>
2023-02-20 23:36:39 +0100

    commit

--- challenge-4/script.js
+++ challenge-4/script.js
@@ -1,20 +1,43 @@
 const container = document.getElementById("container");
-let height = screen.height * 2;
+const sprite = [
+  [0, 0, 1, 1, 1, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1, 1],
+  [0, 1, 0, 0, 0, 0, 1, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0]
+];
 
-for (i = 0; i < height; i++) {
+console.table(sprite[1])
+
+const colors = {
+  0: "grey",
+  1: "black"
+};
+
+console.table(colors)
+
+const height = window.innerHeight * 1.7;
+
+for (let i = 0; i < height; i++) {
   const block = document.createElement("div");
-  block.className = "block";
+  block.className = `block i${i}`;
   container.appendChild(block);
 }
 
-let activities = [
-  ["Work", 9],
-  ["Eat", 1],
-  ["Commute", 2],
-  ["Play Game", 1],
-  ["Sleep", 7],
-];
-activities.push(['Study',2]);
+// Calculate the starting row and column for the sprite
+let startRow = 690
+let startCol = 699
 
-console.table(activities);
-console.log(activities[0][1]);
+// Loop through the sprite array and create the grid layout
+for (let i = 0; i < sprite.length; i++) {
+  startRow = startRow + 1;
+  const row = document.querySelector(`.i${startRow}`)
+
+  if(colors[0] == "grey"){
+    row.style.background= "grey"
+  }
+
+}
